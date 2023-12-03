fffcb879596e34aefb73003b5f9b06586af18740
Gabriel Sicurella <78038697+Gabaee@users.noreply.github.com>
2023-12-03 18:38:15 -0500

    Update Board.java

--- GameofNim/Board.java
+++ GameofNim/Board.java
@@ -1,9 +1,28 @@
 import java.util.Random;
-public class Board
-{
-    public static int populate()
-    {
+public class Board {
+    private static int pieces;
+    private static int maxGuess;
+
+    public static void populate() {
         Random pileSize = new Random();
-        return pileSize.nextInt(50 - 10 + 1) + 10;
+        pileSize.nextInt(50 - 10 + 1);
+    }
+
+    public static int getPieces() {
+        return pieces;
+    }
+
+    public static int getMaxGuess() {
+        return maxGuess;
+    }
+
+    public static void removePieces(int p) {
+        pieces -= p;
+    }
+
+    public static void setMaxGuess()
+    {
+        maxGuess = (int) (pieces / 2);
+        if (pieces == 1) { maxGuess = 1; }
     }
 }
