faf92b65c027564f097249b9aa4223ce4427f7b4
Gabriel Sicurella <78038697+Gabaee@users.noreply.github.com>
2023-12-05 10:35:52 -0500

    Update Player.java

--- GameofNim/Player.java
+++ GameofNim/Player.java
@@ -1,17 +1,13 @@
 import java.util.Scanner;
-public class Player {
+public class Player 
+{
     private final String name;
     private int score;
+
     public Player()
     {
         Scanner sc = new Scanner(System.in);
         System.out.println("Please enter your name: "); name = sc.next();
-        System.out.println("Welcome to the Game of Nim!!");
-        score = 0;
-    }
-    public Player(String name)
-    {
-        this.name = name;
         score = 0;
     }
     public String getName()
@@ -26,6 +22,4 @@ public class Player {
     {
         this.score += 1;
     }
-
-
 }
